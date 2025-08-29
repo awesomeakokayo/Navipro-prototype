454294ea4ebbd91e239dd8c9512d383e21fc0ef0
awesomeakokayo <awesomeakokayo@gmail.com>
2025-08-29 03:31:52 -0700

    New Repo to host Navipro frontend officially

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function onSubmit(token) {
+  document.getElementById("hero-form").submit();
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.getElementById("hamburger");
   const navLinks = document.getElementById("navLinks");
